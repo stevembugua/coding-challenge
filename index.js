429dc546cec7d76772962adf4b32769f8b3663e2
stevembugua <113033981+stevembugua@users.noreply.github.com>
2022-11-28 13:57:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ submitSpeed.addEventListener("click",()=>{
 });
 
 const speedDetector = (speed) => {
-  //checks if speed is above 12points
+  if(!(speed<0)){
+    //checks if speed is above 12points
   if ((speed - 70) / 5 > 12) {
     alert("licence suspended");
   }
@@ -16,5 +17,6 @@ const speedDetector = (speed) => {
     //returns a point is speed is above the required by 5km/h
   } else {
     alert("points: " + (speed - 70) / 5);
-  }
+  }else{
+    alert("Invalid speed.Input is from 0km/h")
 };
